1df3d6b58e7e44c0a09e28985d1dff7af1983183
Vishesh Goyal <visheshgoyal@Visheshs-Macbook.local>
2019-09-14 16:34:22 -0700

    files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] arg) {
+<<<<<<< Updated upstream
         Board b = new Board();
         b.print();
+=======
+        System.out.println("Hello  World")
+        //Board b = new Board();
+        //b.print();
+>>>>>>> Stashed changes
     }
 }
