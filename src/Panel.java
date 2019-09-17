b35dc712cfa79bcbddbdd4664f5b07686ce91f0c
Vishesh Goyal <visheshgoyal@VisheshsMacbook.hsd1.ca.comcast.net>
2019-09-16 21:24:42 -0700

    Merging Running branch into master

--- src/Panel.java
+++ src/Panel.java
@@ -1,10 +1,31 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Panel extends JPanel {
 
-    public void paint(Graphics g) {
-        super.paint(g);
+    public static BufferedImage fence;
+    public static BufferedImage mho;
+    public static BufferedImage player;
+
+    public Panel() {
+        super();
+        try {
+            fence = ImageIO.read(new File("Fence.png"));
+            mho = ImageIO.read(new File("Mho.png"));
+            player = ImageIO.read(new File("Player.png"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+
 
     }
 }
